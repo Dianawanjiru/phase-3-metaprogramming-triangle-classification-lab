e1787d0b124e8b44794d339bb01b1335fbe02adf
Diana <diana.wanjiru@student.moringaschool.com>
2022-10-24 14:59:12 +0300

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind
+    valid_triangle  
+
+    if a==b && b==c
+      :equilateral
+    elsif a!=b && b!=c && a!=c
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def triangle_inequality?
+    a+b>c && a+c>b && c+b>a
+  end
+
+  def positive_triangle?
+    [a,b,c].all?(&:positive?)
+  end
+
+  def valid_triangle
+   raise TriangleError unless triangle_inequality? && positive_triangle?
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
+  
 end
